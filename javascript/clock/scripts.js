b6b0e9655d0f7e206582be4ef8ebc8fa655b53bc
a1rwolff <andy.wolff@martinwilliams.com>
2019-07-10 13:50:22 -0500

    my solutions

--- javascript/clock/scripts.js
+++ javascript/clock/scripts.js
@@ -1,13 +1,21 @@
 
 const secondHand = document.querySelector('.secondHand');
+const minHand = document.querySelector('.minHand');
+const hourHand = document.querySelector('.hourHand')
 
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hour = now.getHours();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minDegrees = ((minutes / 60) * 360) + 90;
+    const hourDegrees = ((hour / 60) * 360) + 30;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds + " " + now);
+    minHand.style.transform = `rotate(${minDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;s
+    console.log(seconds + " " + minutes + " " + hour);
 }
 
 setInterval(setDate, 1000);
