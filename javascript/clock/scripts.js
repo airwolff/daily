5489d07012979cbeabd33a475487cf17a33f7e47
a1rwolff <andy.wolff@martinwilliams.com>
2019-07-10 13:59:46 -0500

    small fix to degree transition

--- javascript/clock/scripts.js
+++ javascript/clock/scripts.js
@@ -11,7 +11,7 @@ function setDate() {
     const hour = now.getHours();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     const minDegrees = ((minutes / 60) * 360) + 90;
-    const hourDegrees = ((hour / 60) * 360) + 30;
+    const hourDegrees = ((hour / 12) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     minHand.style.transform = `rotate(${minDegrees}deg)`;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;s
