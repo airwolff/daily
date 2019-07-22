4c5376c6565f6a2186d69ab906a0a9adc41df6a9
a1rwolff <andy.wolff@martinwilliams.com>
2019-07-22 15:06:20 -0500

    starting array methods

--- notes/higher_level.js
+++ notes/higher_level.js
@@ -48,11 +48,11 @@ return squaredIntegers;
 const realargsberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 const squareList = arr => {
   'use strict';
-  // change code below this line
+
   const squaredIntegers = arr.filter(argsber => argsber > 0);
 
   squaredIntegers.map(square => Math.pow(square, 2));
-  // change code above this line
+
   return squaredIntegers;
 };
 // test your code
--- notes/thirty-notes.js
+++ notes/thirty-notes.js
@@ -5,3 +5,21 @@ function handleUpdate() {
     // this.name and this.value returns name="potato" and value attribute from html
   console.log(this.dataset);
 }
+
+
+
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+];
+
+// filter iterates over each object in inventors array and runs callback function on it
+const fifteen = inventors.filter(function(inventor) {
+  if(inventor.year >= 1500 && inventor.year <= 1599) {
+    return true;
+  }
+});
+console.table(fifteen);
