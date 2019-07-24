9c26bdc9d9d0918df850be08de736cd4618c26ba
a1rwolff <andy.wolff@martinwilliams.com>
2019-07-24 15:58:12 -0500

    beginning files put togetehr

--- react/todo_list/src/index.js
+++ react/todo_list/src/index.js
@@ -6,7 +6,6 @@ import Footer from './components/Footer';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Footer />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
