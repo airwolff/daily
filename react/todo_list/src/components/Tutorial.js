d2f5e881b4eba5442ca042ec092821542fe4e0fc
a1rwolff <andy.wolff@martinwilliams.com>
2019-07-28 18:23:23 -0500

    react tutorial

---
+++ react/todo_list/src/components/Tutorial.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Tutorial() {
+    return (
+        <div class="tutorial">
+            <h1>
+                Andy Wolff
+            </h1>
+            <p>
+                Lots of interesting and fascinating information that will wow the masses.
+            </p>
+            <ul>
+                <li>
+                    Something interesting
+                </li>
+                <li>
+                    Something very interesting
+                </li>
+                <li>
+                    Something super interesting
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Tutorial;
