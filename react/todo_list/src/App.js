c1d9a61036dad448e10fa345b75335dea621b1f7
a1rwolff <andy.wolff@martinwilliams.com>
2019-07-29 21:03:52 -0500

    added test data and started items

--- react/todo_list/src/App.js
+++ react/todo_list/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Tutorial from './components/Tutorial';
-import Footer from './components/Footer';
+import TodoItem from "./components/TodoItem"
+import todosData from ".components/todosData"
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Tutorial />
-      <Footer />
+      <TodoItem />
+      <todosData />
     </div>
   );
 }
